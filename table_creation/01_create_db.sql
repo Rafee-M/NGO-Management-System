bddc8b1fe1ced149334085c672a7ba631ba8d76c
Rafee-M <rafee66@proton.me>
2025-12-05 21:21:30 +0600

    Removed files

--- table_creation/01_create_db.sql
+++
@@ -1,8 +0,0 @@
--- ============================================
--- File: 01_create_database.sql
--- Purpose: Create database and set context
--- Execution Order: Run this first
--- ============================================
-
-CREATE DATABASE IF NOT EXISTS ngo_management;
-USE ngo_management;
